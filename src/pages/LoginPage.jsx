8f88dec0c1bb07fbc21a8e1a745aecb17bc7c7f4
nGhart <naaghart@gmail.com>
2023-11-02 11:41:19 +0000

    navigation

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -29,7 +29,7 @@ const LoginPage = () => {
           toast.success(resp.data.msg);
           localStorage.setItem("logged", resp.data.user);
           localStorage.setItem("id", resp.data.staffId);
-          navigate("/dashboard");
+          navigate("/");
         } else {
           toast.error(resp.data.msg);
         }
